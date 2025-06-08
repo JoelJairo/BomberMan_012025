1681b159f67ce33b6ce3ca1b1c428857975b09ef
Joel Choque <jchoquegorena@gmail.com>
2025-06-07 22:31:33 -0400

    LABORATORIO 06 Completado
    > Patrón Factory method, que permita crear todos los tipos de bloques, considerando varios tipos.
    > Cada bloque debe ser almacenada en un TMap o un TArray, para almacenar como clave el numero de bloque que debe ser único entre todas las que se creen.
    > Se debe dar a cada bloque un comportamiento especifico individual y de grupo.
    > Se debe implementar una propiedad tipo de bloque, que debe ser de tipo enumeración, para trabajar con los diferentes tipos de bloques.

--- Source/BomberMan_012025/BomberMan_012025GameMode.cpp
+++ Source/BomberMan_012025/BomberMan_012025GameMode.cpp
@@ -6,10 +6,15 @@
 
 ABomberMan_012025GameMode::ABomberMan_012025GameMode()
 {
-	// set default pawn class to our Blueprinted character
+	// set default pawn class to Blueprint'/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter.ThirdPersonCharacter'
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
-	if (PlayerPawnBPClass.Class != NULL)
+	if (PlayerPawnBPClass.Class != nullptr)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
 	}
+	else
+		{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerPawnBPClass not found! Please check the path."));
+	}
+
 }
